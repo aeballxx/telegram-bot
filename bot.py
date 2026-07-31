c983e6c1ec692b8962cadba696122b642882ec85
aeballxx <iqbalidaham0701@gmail.com>
2026-08-01 06:19:32 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -239,8 +239,6 @@ if step == "name":
 
     USER_DATA[user_id]["step"] = "pin"
 
-    USER_DATA[user_id]["step"] = "pin"
-
 elif step == "pin":
 
         USER_DATA[user_id]["pin"] = update.message.text
